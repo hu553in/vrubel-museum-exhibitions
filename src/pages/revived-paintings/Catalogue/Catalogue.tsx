6ef1568c4c453c3c74ef341f6dfa9e97d348e31f
Ruslan Khasanshin <r.m.khasanshin@gmail.com>
2026-07-30 01:10:54 +0600

    fix: update React Router

--- src/pages/revived-paintings/Catalogue/Catalogue.tsx
+++ src/pages/revived-paintings/Catalogue/Catalogue.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router';
 
 import pictures from '@/assets/revived-paintings/pictures';
 import Loading from '@/components/common/Loading/Loading';
